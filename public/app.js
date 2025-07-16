c2f41b27343f512c48546bf9538cf46c62d2fac7
shivangichandra <se.schandra@googlemail.com>
2025-07-16 16:10:12 +0100

    spell check set to false

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ let words = [];
 let practiceWords = [];
 let current = 0;
 let userAnswers = [];
-const dictationLength = [2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+const dictationLength = [2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
 const startBtn = document.getElementById('startBtn');
 const wordCountSelect = document.getElementById('wordCountSelect');
